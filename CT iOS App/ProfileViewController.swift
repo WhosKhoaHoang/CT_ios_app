2e85101d031ac0bdd4999124dcf7ec4daf72b71f
Khoa Hoang <whoskhoahoang@gmail.com>
2017-05-28 19:13:22 -0700

    Implemented layout for Profile and got the hang of how to style Views.

--- CT iOS App/ProfileViewController.swift
+++ CT iOS App/ProfileViewController.swift
@@ -1,27 +1,27 @@
 //
-//  HomeViewController.swift
+//  ProfileViewController.swift
 //  CT iOS App
 //
-//  Created by Khoa Hoang on 5/27/17.
+//  Created by Khoa Hoang on 5/28/17.
 //  Copyright © 2017 KhoaHoang. All rights reserved.
 //
 
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
-    let homeMenuItems = ["Knowledge Base",
-                         "Drop-Off Signature",
-                         "Opening Store Checklist",
-                         "Office Supplies Checklist",
-                         "Yelp Response Template",
-                         "Worksheets",
-                         "Inventory Database",
-                         "Technician Policies",
-                         "Closing Store Checklist",
-                         "Customer Dialogue"]
+    let profileMenuItems = ["Opening Store Checklist",
+                         "Shipping Procedure",
+                         "Smart Pick-Up Procedure",
+                         "Onsite Phone Repair",
+                         "Customer Procedures",
+                         "Purchasing Procedure",
+                         "Inventory Top Off Procedure",
+                         "Office Supply Checklist",
+                         "Online Marketing",
+                         "Worksheets"]
     
     
     override func viewDidLoad() {
@@ -31,11 +31,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         //This is for not displaying empty rows:
         tableView.tableFooterView = UIView(frame: .zero)
-
         tableView.backgroundColor = UIColor.black
-        
-        print("LOADED HOMEVIEW")
-        
     }
 
     
@@ -43,7 +39,8 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
     //Specify number of sections
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -52,19 +49,23 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
     }
     
-
+    
+    
     //Specify number of rows in a section
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return homeMenuItems.count
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return profileMenuItems.count
     }
+    
     
     
     //Configure each cell
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                
-        let cell = tableView.dequeueReusableCell(withIdentifier: "homeItem", for: indexPath)
         
-        cell.textLabel?.text = homeMenuItems[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileItem", for: indexPath)
+        
+        
+        cell.textLabel?.text = profileMenuItems[indexPath.row]
         cell.textLabel?.textAlignment = .center
         
         cell.textLabel?.textColor = UIColor.green
@@ -75,51 +76,22 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.separatorInset = UIEdgeInsets.zero
         cell.layoutMargins = UIEdgeInsets.zero
         
-        
-        
         return cell
     }
-    
+
     
     //When user selects a row
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         tableView.deselectRow(at: indexPath, animated: true)
         
-        switch (indexPath.row) {
-        case 0:
-            performSegue(withIdentifier: "kbSeg", sender: indexPath)
-        case 1:
-            performSegue(withIdentifier: "doSigSeg", sender: indexPath)
-        case 2:
-            performSegue(withIdentifier: "openStoreCLSeg", sender: indexPath)
-        case 3:
-            performSegue(withIdentifier: "officeSupCLSeg", sender: indexPath)
-        case 4:
-            performSegue(withIdentifier: "yelpRespSeg", sender: indexPath)
-        case 5:
-            performSegue(withIdentifier: "worksheetsSeg", sender: indexPath)
-        case 6:
-            performSegue(withIdentifier: "invenDbSeg", sender: indexPath)
-        case 7:
-            performSegue(withIdentifier: "techPolSeg", sender: indexPath)
-        case 8:
-            performSegue(withIdentifier: "closeStoreClSeg", sender: indexPath)
-        case 9:
-            performSegue(withIdentifier: "custDiaSeg", sender: indexPath)
-        default:
-            break
-        }
-        
     }
     
-
+    
     //For adjusting height of a cell
     public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
-    
-    
     /*
     // MARK: - Navigation
 
