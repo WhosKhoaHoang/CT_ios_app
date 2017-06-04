87b42b32f5aee722199da1ca49486002b84d8e98
Khoa Hoang <whoskhoahoang@gmail.com>
2017-06-04 16:58:36 -0700

    Implemented validation for the Receive Signature page.

--- CT iOS App/ReceiveDeviceViewController.swift
+++ CT iOS App/ReceiveDeviceViewController.swift
@@ -22,14 +22,75 @@ class ReceiveDeviceViewController: UIViewController, SwiftSignatureViewDelegate 
     @IBOutlet weak var custNameInput: UITextField!
     @IBOutlet weak var signature: SwiftSignatureView!
     @IBOutlet weak var receivedByInput: UITextField!
+    @IBOutlet weak var sigDatePicker: UIDatePicker!
     @IBOutlet weak var theContent: UIView!
     //^This is for the PDF
+    @IBOutlet weak var submitBtn: UIButton!
+    
     
     @IBAction func submit(_ sender: Any) {
+
+        let errorMsg: String = validateSigPage()
         
-        
+        if (errorMsg != "") {
+            createAlert(titleText: "Error", msgText: errorMsg)
+        }
+        else {
+            print("TO THE DROPBOX!")
+        }
         
     }
+    
+    
+    func validateSigPage()->String {
+        
+        var errorMsg: String = ""
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "M/d/yy"
+        
+        let sigDate = dateFormatter.string(from: sigDatePicker.date)
+        let curDate = dateFormatter.string(from: Date())
+        
+        if (workOrderNumInput.text == "") {
+            errorMsg += ". Please enter a Work Order #\n"
+        }
+
+        if (!cb1.isChecked || !cb2.isChecked || !cb3.isChecked) {
+            errorMsg += ". Please agree to all the terms\n"
+        }
+        
+        if (custNameInput.text == "") {
+            errorMsg += ". Please enter customer's name\n"
+        }
+        
+        if (signature.signature == nil) {
+            errorMsg += ". Please provide customer's signature\n"
+        }
+        
+        if (receivedByInput.text == "") {
+            errorMsg += ". Please specify who received the signature\n"
+        }
+        
+        if (sigDate != curDate) {
+            errorMsg += ". Please enter today's date\n"
+        }
+     
+        return errorMsg
+        
+    }
+    
+    
+    func createAlert(titleText: String, msgText: String) {
+        
+        let alert = UIAlertController(title: titleText, message: msgText, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     
     
     
@@ -37,6 +98,9 @@ class ReceiveDeviceViewController: UIViewController, SwiftSignatureViewDelegate 
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        submitBtn.layer.cornerRadius = 5
+        //submitBtn.layer.borderWidth = 1
+        //submitBtn.layer.borderColor = UIColor.black.cgColor
     }
     
     override func didReceiveMemoryWarning() {
@@ -45,23 +109,14 @@ class ReceiveDeviceViewController: UIViewController, SwiftSignatureViewDelegate 
     }
     
     
-    //Don't need this
-    /*
-     //For if you have a clear button. Be sure to clear the SignatureVIew after submitting!
-     @IBAction func didTapClear() {
-     signatureView.clear()
-     }
-     */
-    
-    
     //MARK: Delegate
     public func swiftSignatureViewDidTapInside(_ view: SwiftSignatureView) {
-        print("Did tap inside")
+        //print("Did tap inside")
     }
     
     
     public func swiftSignatureViewDidPanInside(_ view: SwiftSignatureView) {
-        print("Did pan inside")
+        //print("Did pan inside")
     }
     
     /*
